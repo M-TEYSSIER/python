be14b7bf91d108e86a3767db87b9f2b100d71c03
M-TEYSSIER <maxime.teyssier@laposte.net>
2021-08-09 18:35:39 +0200

    Add logging text

--- irc_server.py
+++ irc_server.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8
 
 import socket, os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 SOCKET_LINK = "/home/maxime/irc.socket"
 server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -10,21 +13,26 @@ class IRC_Server:
     def __init__():
         if os.path.exists(SOCKET_LINK):
             os.remove(SOCKET_LINK)
+            logging.info("Deleting the socket already present")
     
     def __disconnect__():
         server.close()
-        print("Disconnected")
+        logging.info("Disconnection to the server")
 
     def __socket__(server):
         server.bind(SOCKET_LINK)
         server.listen(5)
+        logging.info("Listening on the socket")
 
     def client(self):
         client, _ = self.accept()
+        logging.info("Accepted client")
         datagram = client.recv(1024)
         print("-" * 20)
         print(datagram.decode('utf-8'))
+        logging.info("Data received: {%s}", datagram.decode("utf-8"))
         print("-" * 20)
+
 
 def main():
     irc = IRC_Server
