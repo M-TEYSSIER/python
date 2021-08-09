e2bb2b2db27913992ade700ef991105eebf71fdd
M-TEYSSIER <maxime.teyssier@laposte.net>
2021-08-09 16:52:21 +0200

    Create class with a few attributes

---
+++ compte.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding utf-8
+
+class compte:
+    def __init__(self, pseudo, nom, prenom, age):
+        self.pseudo = pseudo
+        self.nom = nom
+        self.prenom = prenom
+        self.age = age
