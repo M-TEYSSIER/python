8db7d2a0a070eb8b051bde976b4cac7f3eef2634
M-TEYSSIER <maxime.teyssier@laposte.net>
2021-08-09 16:59:49 +0200

    Add args parser

--- irc_client.py
+++ irc_client.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8
 
-import socket, os, sys
 from compte import compte
+import socket
+import argparse
+import logging
 
 SOCKET_LINK = "/home/maxime/irc.socket"
 
 class IRC_client:
     def __init__(client):
+        #TODO: Why problem pop during connection on IF condition
         if client.connect(SOCKET_LINK):
             print("Connected to server")
         else:
             print("Problem to connect")
-
+        
     def Sending_data(client, data):
         client.send(data.encode("utf-8"))
         client.close()
 
     def message(user, msg):
-        data = user.pseudo + ": " + msg
+        data = user.pseudo + "(" + user.nom + "_" + user.prenom + "_" + str(user.age) + ")" + ": " + msg
         print(data)
         return data
 
@@ -31,8 +34,20 @@ def main(user, msg):
     message = IRC_client.message(user, msg)
     IRC_client.Sending_data(client, message)
 
-
 if __name__ == "__main__":
-    user = compte("Riesjack", "TEYSSIER", "Maxime", 26)
-    msg = str(sys.argv[1:]).replace("['","").replace("']","")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--message", help="Message to send to IRC server")
+    parser.add_argument("-u", "--username", help="Username identification")
+    parser.add_argument("-ln", "--last-name", help="Last name of writer")
+    parser.add_argument("-fn", "--first-name", help="First name of writer")
+    parser.add_argument("-a", "--age", help="Age of writer", type=int)
+    args=parser.parse_args()
+    msg = args.message
+
+    user = compte(
+                args.username, 
+                args.last_name,
+                args.first_name, 
+                args.age)
+
     main(user, msg)
