b481314156dff5ea72c9d404e729eeb007aa7805
M-TEYSSIER <maxime.teyssier@laposte.net>
2021-09-19 21:31:17 +0200

    Update command for add complexity to use

--- bot_discord/bot.py
+++ bot_discord/bot.py
@@ -10,20 +10,9 @@ import sqlite3
 class MyClient(discord.Client):
     default_intents = discord.Intents.default()
     default_intents.members=True
-
     client = discord.Client(intents=default_intents)
 
-    cmd=['!help', '!list', '!add', '!remove']
-
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('-------------------')
-        print("Server with bot:")
-        for guild in client.guilds:
-            print(guild)
-        print('-------------------')
+    cmd=['~help', '~list', '~add', '~remove']
 
     if not os.path.exists("ban_words.sqlite"):
         condb = sqlite3.connect("ban_words.sqlite", timeout=1000)
@@ -38,58 +27,15 @@ class MyClient(discord.Client):
         condb = sqlite3.connect("ban_words.sqlite", timeout=1000)
         con = condb.cursor()
 
-
-    @client.event
-    async def on_member_join(member):
-        pass
-
-    @client.event
-    async def on_message(self, message):
-        msg = message.content
-
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        # Print help menu
-        if msg.startswith("!help"):
-            await message.channel.send("```\
-                FR:\
-                \nLe bot FlashModz a pour but de vérifier les messages du chat\
-                \net de les supprimer si des mots ne sont pas appropriés.\
-                \n!help: Affiche les menu d'aide.\n!list: Affiche les mots non appropriés\
-                 présent dans la base de données.\
-                \n!add <mot>: Ajout le mot dans la base de données.\
-                \n!remove <word>: Supprime le mot de la base de données.\
-                \nEN:\
-                \nThe FlashModz bot have purpose to \
-                check new messages on chat and delete if they not appropriated.\
-                \n\n!help: Print the help menu.\n!list: Print the ban words list.\
-                registered on database.\
-                \n!add <word>: Add single word on database.\
-                \n!remove <word>: Delete word from the database.```")
-
-        # !list show ban words dictionnary
-        if msg.startswith("!list"):
-            liste = self.listing()
-            await message.channel.send("Banned words list:")
-            await message.channel.send(f'```{liste}```')
-
-        # Append ban word on dictonnaty
-        if msg.startswith("!add"):
-            word = msg.replace("!add ","")
-            self.add_bw(word)
-
-        # Remove word from the dictonnary
-        if re.search("!remove", str.lower(msg)):
-            word = msg.replace("!remove ","")
-            self.del_bw(word)
-
-        # Delete message with ban word on chat
-        if not any(message.content.startswith(word) for word in self.cmd):
-            if any(word in list(message.content.lower().split(" ")) for word in  self.listing()):
-                await message.delete()
-                await message.channel.send(f"WARNING: {message.author.mention}, surveille ton langage !")
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('-------------------')
+        print("Server with bot:")
+        for guild in client.guilds:
+            print(guild)
+        print('-------------------')
 
     def add_bw(self, word):
         try:
@@ -113,9 +59,56 @@ class MyClient(discord.Client):
             result.append(e[0])
         return result
 
+    @client.event
+    async def on_member_join(member):
+        pass
+
+    @client.event
+    #@commands.has_role()
+    async def on_message(self, message):
+        msg = message.content
+
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        # Print help menu
+        if msg.startswith("~show_help"):
+            await message.channel.send("```\
+                FR:\
+                \nLe bot FlashModz a pour but de vérifier les messages du chat et de les supprimer si des mots ne sont pas appropriés.\
+                \n~show_help: Affiche les menu d'aide.\n~show_list: Affiche les mots non appropriés présent dans la base de données.\
+                \n~add_ban_word <mot>: Ajout le mot dans la base de données.\
+                \n~remove_ban_word <mot>: Supprime le mot de la base de données.\
+                \nEN:\
+                \nThe FlashModz bot have purpose to check new messages on chat and delete if they not appropriated.\
+                \n~show_help: Print the help menu.\n~show_list: Print the ban words list registered on database.\
+                \n~add_ban_word <word>: Add single word on database.\
+                \n~remove_ban_word <word>: Delete word from the database.```")
+
+        # !list show ban words dictionnary
+        if msg.startswith("~show_list"):
+            liste = self.listing()
+            await message.channel.send("Banned words list:")
+            await message.channel.send(f'```{liste}```')
+
+        # Append ban word on dictonnaty
+        if msg.startswith("~add_ban_word"):
+            word = msg.replace("~add_ban_word ","")
+            self.add_bw(word)
+
+        # Remove word from the dictonnary
+        if re.search("~remove_ban_word", str.lower(msg)):
+            word = msg.replace("~remove_ban_word ","")
+            self.del_bw(word)
+
+        # Delete message with ban word on chat
+        if not any(message.content.startswith(word) for word in self.cmd):
+            if any(word in list(message.content.lower().split(" ")) for word in  self.listing()):
+                await message.delete()
+                await message.channel.send(f"WARNING: {message.author.mention}, surveille ton langage !")
 
 
 load_dotenv(dotenv_path="config")
-
 client = MyClient()
 client.run(os.getenv("FLASHMODZ_BOT_TEST"))
