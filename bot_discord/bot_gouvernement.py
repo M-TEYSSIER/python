50c1f6d2c8d43ec4a28347ba3d82efbda8faef3b
M-TEYSSIER <maxime.teyssier@laposte.net>
2021-10-27 00:30:01 +0000

    First commit with explenations of project

---
+++ bot_discord/bot_gouvernement.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python3
+# -*- coding utf-8 -*-
+
+# Created for GTA Flashmodz RP french server (https://discord.gg/F4B7NAQE9S)
+# in order to calculate governental's tax for LS's citizens 
